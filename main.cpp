79e2ae129a35920d90a2ba382193bcda34789876
Olimjon <olimjon.nematov@gmail.com>
2020-04-19 18:31:25 -0500

    created the vector to hold each line from file

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include "Ship.h"
+#include <vector>
 
 using namespace std;
 
@@ -15,13 +16,24 @@ int main() {
 
     //if word ==enter then create new ships
    string tempString="";
+
+
     //get each line
     while(getline(in,tempString)){
-        //for each line, parse the string and find the first word
-        string tempFirstWord=tempString.substr(0,tempString.find_first_of(' '));
+        //create a vector of each word in the string
+        vector <string> wordsInEachLine;
+        //fill the vector for each stirng
+        int prevSpace=-1;
+        for(int i=0;i<tempString.length();i++){
+            if(tempString.at(i)==' '){
+                wordsInEachLine.push_back(tempString.substr(prevSpace+1,i-prevSpace-1));
+                prevSpace=i;
+            } else if(i==tempString.size()-1){
+                wordsInEachLine.push_back(tempString.substr(prevSpace+1,i-prevSpace));
+            }
+        }
+        if(wordsInEachLine.at(0)=="enter"){
 
-        if(tempFirstWord=="enter"){
-            
             //create a new ship object
             Ship *tempShip=new Ship();
 
@@ -29,7 +41,7 @@ int main() {
 
             //if dock is full, add ship to linked list
         }
-        if(tempFirstWord=="exit"){
+        if(wordsInEachLine.at(0)=="exit"){
             //remove the obj from the dock
 
             //check if ticket should be generated
